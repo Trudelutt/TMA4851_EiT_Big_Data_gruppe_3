c68b40bac0c361e4f4821f9961e76a4083030afc
Ella-Lovise <elrorvik@stud.ntnu.no>
2018-02-17 13:40:04 +0100

    fixed something in com_back.js

--- communication_back_front/com_back.js
+++ communication_back_front/com_back.js
@@ -1,27 +1,30 @@
 // node start.js
 
 // initialize variables
-var spawn = require('child_process').spawn,
-py    = spawn('python3', ['com_front.py']),
-data_list = [1,2,3,4,5,6,7,8,9],
-data_from_py = [];
+function new_request(command){
+  var spawn = require('child_process').spawn,
 
+  // start python process
+  py    = spawn('python3', ['com_front.py']),
+  data_from_py = [];
 
-commandDict = {'type':'CountryLatLon'}
+  //write to python
+  command_msg = {'type':command}
+  py.stdin.write(JSON.stringify(command_msg));
 
-/*We have to stringify the data first otherwise our python process wont recognize it*/
-py.stdin.write(JSON.stringify(commandDict));
+  // recive data from python as variable data_from_py
+  py.stdout.on('data', function(data){
+    data_from_py = JSON.parse(data);
+  });
 
-// recive data from python as variable data
-py.stdout.on('data', function(data){
-  data_from_py = JSON.parse(data);
-});
+  /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+  py.stdout.on('end', function(){
+    for (var i = 0; i < data_from_py['country'].length ; i++) {
+      console.log(data_from_py['country'][i],data_from_py['lat'][i],data_from_py['lon'][i] )
+    }
+  });
 
-/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
-py.stdout.on('end', function(){
-  for (var i = 0; i < data_from_py['country'].length ; i++) {
-    console.log(data_from_py['country'][i],data_from_py['lat'][i],data_from_py['lon'][i] )
-  }
-});
+  py.stdin.end();
+}
 
-py.stdin.end();
+new_request('CountryLatLon');
