50ef3933a88d0a17553c1af11dec336b73e2ffba
sigvekb <sigvekb@gmail.com>
2018-02-21 10:36:04 +0100

    Sigvefuckeralt

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Wrapper, GridWrapper, App, Visualization } from './elements';
+import { slide as Menu } from 'react-burger-menu'
 
 export default class Hello extends Component {
   render() {
     return (
       <Wrapper>
+        <Menu>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </Menu>
         <GridWrapper>
-          <Visualization data={['test']} />
+          <Visualization data={['Hva skjer?']} />
         </GridWrapper>
         <App />
       </Wrapper>
@@ -15,4 +22,6 @@ export default class Hello extends Component {
   }
 }
 
+
 render(<Hello />, document.getElementById('app'));
+//render(<Example />, document.getElementById('app'));
