96ac495a62178f5a37e21e22850fe227daae380d
danielhalvorsen <daniel.oe.halvorsen@gmail.com>
2018-02-28 16:16:06 +0100

    Update lokalitet.py after EiT dag 8 :)

--- lokalitet/lokalitet.py
+++ lokalitet/lokalitet.py
@@ -19,9 +19,32 @@ def avstand(land1,land2):
     
 #avstand = avstand('NOR','USA')
 
+def alpha3ToCountryName(alpha):
+	try:
+		country = pycountry.countries.get(alpha_3=alpha)
+	except KeyError:
+		print("Could not recognise code - default value returned")
+		country = pycountry.countries.get(alpha_3="ATA")
+	countryName = country.name
+	return countryName
 
-def TM_vare_import_og_eksport(df,land1,land2,item):
-    
+def countryNameToAlpha3(cName):
+	try:
+		country = pycountry.countries.get(name=cName)
+	except KeyError:
+		print("Could not recognise code - default value returned")
+		country = pycountry.countries.get(name="Antarctica")
+	alpha = country.alpha_3
+	return alpha
+
+#print(alpha3ToCountryName("NOR"))
+#print(countryNameToAlpha3("Norway"))
+
+
+def TM_vare_import_og_eksport(land1,land2,item):
+    df=pd.read_csv('TM.csv', sep=',',header=None,encoding='latin-1')
+    df = df.values
+
     
     import_Tonn_Array = np.array([])
     eksport_Tonn_Array = np.array([])
@@ -29,26 +52,30 @@ def TM_vare_import_og_eksport(df,land1,land2,item):
         if(df[i][1]==land1 and df[i][3]==land2 and df[i][5]==item and df[i][7]=='Import Quantity'):
             for k in range(1,56,2):
                 import_Tonn_Array = np.append(import_Tonn_Array,df[i][-(k+1)])
-        
-    for i in range(len(df)):
         if(df[i][1]==land1 and df[i][3]==land2 and df[i][5]==item and df[i][7]=='Export Quantity'):
             for k in range(1,56,2):
                 eksport_Tonn_Array = np.append(eksport_Tonn_Array,df[i][-(k+1)])
-            #eksport_Tonn_Array = df[i][-2]
-    for i in range(len(import_Tonn_Array)):
+    if(not import_Tonn_Array.any):
+       import_Tonn_Array = np.zeros(28)
+       
+       print('error, making zero vector')
+  
+    if(not eksport_Tonn_Array.any):  
+        eksport_Tonn_Array = np.zeros(28)
+        print('error, making zero vector')
+    maxLen = np.max(len(import_Tonn_Array),len(eksport_Tonn_Array))
+    for i in range(maxLen):
         if math.isnan(import_Tonn_Array[i]):
             import_Tonn_Array[i]=0
         if math.isnan(eksport_Tonn_Array[i]):
             eksport_Tonn_Array[i]=0
     eksport_Tonn_Array = np.flip(eksport_Tonn_Array,0)
     import_Tonn_Array = np.flip(import_Tonn_Array,0)
-    return import_Tonn_Array,eksport_Tonn_Array
+    return df,import_Tonn_Array,eksport_Tonn_Array
 
 
-TM=pd.read_csv('TM.csv', sep=',',header=None,encoding='latin-1')
-TM = TM.values
 
-import_tonn,eksport_tonn = TM_vare_import_og_eksport(TM,'Canada','Republic of Korea','Cereals, breakfast') 
+df,import_tonn,eksport_tonn = TM_vare_import_og_eksport('Spain','Norway','Avocados') 
 
 
 
