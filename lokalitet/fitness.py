0895dce3f0b36851acaf44be0c9a2d8d8d0c8abc
Ella-Lovise <elrorvik@stud.ntnu.no>
2018-02-28 11:03:31 +0100

    working on fitness

---
+++ lokalitet/fitness.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+circumference = 40075017 # in km
+distance_penalty = 1000
+
+def fitness(distance,amount):
+
+
+
+
+def penalty_distance(x):
+
+    return distance_penalty*np.tanh(x) + 1
